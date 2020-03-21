34cd91de5e201d67b8931aec52944d984847f756
amin <amin.karimi@bluebitsoft.com>
2020-03-21 10:58:10 +0430

    add a java sample of adapter pattern

---
+++ Structural Patterns/Adapter/ExampleAdapter.java
@@ -0,0 +1,37 @@
+public Interface CoffeeMachineInterface {
+    public void chooseFirstSelection();
+    public void chooseSecondSelection();
+    public void chooseThirdSelection();
+}
+
+public class CoffeeTouchscreenAdapter extends CoffeeMachineInterface{
+    public CoffeeTouchscreenAdapter(OldCoffeeMachine oldCoffeeMachine){
+        this.oldCoffeeMachine = oldCoffeeMachine;
+    }
+
+    public void chooseFirstSelection() {
+        this.oldCoffeeMachine.selectA();
+    }
+
+    public void chooseSecondSelection(){
+        this.oldCoffeeMachine.selectB();
+    }
+
+    public void chooseThirdSelection(){
+        this.oldCoffeeMachine.selectC();
+    }
+}
+
+public class OldCoffeeMachine {
+    public void selectA(){
+
+    }
+
+    public void selectB(){
+
+    }
+
+    public void selectC(){
+
+    }
+}
